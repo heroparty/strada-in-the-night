25fb92be06594d7aa3bd8068df4a4176e9d51a6c
Diego Prestes <diegoprestes.git@hotmail.com>
2020-10-18 15:32:59 -0300

    feat: rocks rolling, audios addeds, new Strada sprite added

--- src/rocknroll.gd
+++ src/rocknroll.gd
@@ -1,1 +1,20 @@
-extends Node
+extends KinematicBody2D
+
+var speed = 275
+var screensize
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _process(delta):
+	var vel = Vector2()
+	vel.y += speed
+	if vel.length() > 0:
+		var info = move_and_collide(vel * delta)
+		if info:
+			if ('Car' in info.get_collider().name):
+				$audioAcidente.play()
+				hide()
+				#gameoverscreen.show()
+				#contador.parar()
+				#xesque.delhe()
